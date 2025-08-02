40f418a1cf9ef2474d0d828533c03628f456fa89
Виктор <v.shakov@list.ru>
2025-08-02 14:54:49 +0300

    Added solution to the task "Excel table column number".

---
+++ Excel Sheet Column Number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace ExcelSheet
+{
+    public class Program
+    {
+        private static readonly char[] alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+        public static void TitleToNumber()
+        {
+            Console.WriteLine("Input a column title");
+
+            char[] columnTItleArray = Console.ReadLine().ToArray();
+            int columnNumber = 0;
+
+            for (int i = 0; i < columnTItleArray.Length; i++)
+            {
+                int alphabetIndex = alphabet.Select((s, id) => new { s, id }).Where(x => x.s.Equals(columnTItleArray[i])).Select(x => x.id + 1).Single();
+                if (i < columnTItleArray.Length - 1)
+                {
+                    columnNumber += (26 * alphabetIndex);
+                }
+                else
+                {
+
+                    columnNumber += alphabet.Select((s, id) => new { s, id }).Where(x => x.s.Equals(columnTItleArray[i])).Select(x => x.id + 1).Single();
+                }
+         
+            }
+
+            Console.WriteLine(columnNumber);
+            Console.WriteLine("Press X to close app");
+        }
+
+        public static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                TitleToNumber();
+                keyinfo = Console.ReadKey();
+            }
+            while (keyinfo.Key != ConsoleKey.X);
+        }
+    }
+}
